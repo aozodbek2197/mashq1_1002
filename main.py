05b4fad479fb591f2b4acc76b8eedec827875ae5
aozodbek2197 <aozodbek2197@gmail.com>
2026-02-10 14:35:28 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+class Talaba:
+    def __init__(self, ism, familiya, tyil):
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
+
+talaba1 = Talaba("Murodjon", "Atajanov", 2010)
+print(talaba1.ism)
+print(talaba1.familiya)
+print(talaba1.tyil)
